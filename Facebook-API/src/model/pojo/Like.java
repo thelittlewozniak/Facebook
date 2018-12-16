60282ec6a813d648378a39d5f66fb2b037f7053e
thelittlewozniak <nathan.pire@gmail.com>
2018-12-16 01:49:55 +0100

    Creating Like and Friend pojo

---
+++ Facebook-API/src/model/pojo/Like.java
@@ -0,0 +1,47 @@
+package model.pojo;
+
+
+import java.util.Date;
+
+public class Like {
+
+    private Date dateLiked;
+
+    private User user;
+
+    private Post post;
+
+    private Comment comment;
+
+
+    private Date getDateLiked() {
+        return this.dateLiked;
+    }
+
+    private void setDateLiked(Date dateLiked) {
+        this.dateLiked = dateLiked;
+    }
+
+    private User getUser() {
+        return this.user;
+    }
+
+    private void setUser(User user) {
+        this.user = user;
+    }
+
+
+    //                          Operations
+
+    public Like createAPost() {
+        //TODO
+        return null;
+    }
+
+    public boolean deleteAPost() {
+        //TODO
+        return false;
+    }
+
+
+}
