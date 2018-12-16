acdb8ee7435e560ce6c3a3a8552c34bbdb3bdc32
thelittlewozniak <nathan.pire@gmail.com>
2018-12-16 01:55:55 +0100

    Creating DAO factory

---
+++ Facebook-API/src/model/dao/Dao.java
@@ -0,0 +1,35 @@
+package model.dao;
+
+import java.sql.Connection;
+
+public abstract class Dao<T> {
+    protected Connection connect = null;
+
+    public Dao(Connection conn){
+        this.connect = conn;
+    }
+
+    /**
+     * @param obj
+     * @return boolean
+     */
+    public abstract boolean create(T obj);
+
+    /**
+     * @param obj
+     * @return boolean
+     */
+    public abstract boolean delete(T obj);
+
+    /**
+     * @param obj
+     * @return boolean
+     */
+    public abstract boolean update(T obj);
+
+    /**
+     * @param id
+     * @return T
+     */
+    public abstract T find(int id);
+}
