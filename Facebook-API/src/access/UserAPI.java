e046a4133b0945ac108c7f55837d35d7d800d70d
thelittlewozniak <nathan.pire@gmail.com>
2018-12-27 17:45:25 +0100

    Making the delete method and the put method for UserAPI

--- Facebook-API/src/access/UserAPI.java
+++ Facebook-API/src/access/UserAPI.java
@@ -67,7 +67,58 @@ public class UserAPI extends RestApplication{
         u.setPhoneNumber(Integer.parseInt(phoneNumber));
         u.setGender(Boolean.parseBoolean(gender));
         u.setInterestedIn(Boolean.parseBoolean(interestedIn));
-        Boolean test=new DaoUser(conn).create(u);
+        //Boolean test=new DaoUser(conn).create(u);
+        Response response=null;
+        //if(test)
+            response=Response.status(Response.Status.OK).entity(u).build();
+        //else
+            //response=Response.status(Response.Status.BAD_REQUEST).entity(u).build();
+        return response;
+    }
+    @DELETE
+    @Path("DeleteUser")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteUser(@QueryParam("id")int id){
+        Connection conn=GetConnection.getInstance().getConnection();
+        User u=new DaoUser(conn).find(id);
+        Boolean test=new DaoUser(conn).delete(u);
+        Response response=null;
+        if(test)
+            response=Response.status(Response.Status.OK).entity(test).build();
+        else
+            response=Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(null).build();
+        return response;
+    }
+    @PUT
+    @Path("UpdateUser")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateUser(@FormParam("firstname") String firstname,@FormParam("lastname") String lastname,@FormParam("email") String email,@FormParam("password") String password,@FormParam("address") String address,@FormParam("birthday") String birthday,@FormParam("registerDate") String registerDate,@FormParam("relationship") String relationship,@FormParam("phoneNumber") String phoneNumber,@FormParam("gender") String gender,@FormParam("interestedIn") String interestedIn){
+        Connection conn=GetConnection.getInstance().getConnection();
+        User u=new User();
+        u.setFirstname(firstname);
+        u.setLastname(lastname);
+        u.setEmail(email);
+        u.setPassword(password);
+        u.setAddress(address);
+        Date birth=null;
+        try {
+            birth=new SimpleDateFormat("dd/MM/yyyy").parse(birthday);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        u.setBirthday(birth);
+        Date register=null;
+        try {
+            register=new SimpleDateFormat("dd/MM/yyyy").parse(registerDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        u.setRegisterDate(register);
+        u.setRelationship(Boolean.parseBoolean(relationship));
+        u.setPhoneNumber(Integer.parseInt(phoneNumber));
+        u.setGender(Boolean.parseBoolean(gender));
+        u.setInterestedIn(Boolean.parseBoolean(interestedIn));
+        Boolean test=new DaoUser(conn).update(u);
         Response response=null;
         if(test)
             response=Response.status(Response.Status.OK).entity(test).build();
