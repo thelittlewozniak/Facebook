1279f25a99c890b616d5ad84ee6f73f908f1050d
thelittlewozniak <nathan.pire@gmail.com>
2018-12-27 16:54:01 +0100

    making the Post for UserAPI

--- Facebook-API/src/access/UserAPI.java
+++ Facebook-API/src/access/UserAPI.java
@@ -1,5 +1,6 @@
 package access;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import model.BusinessLayer.GetConnection;
 import model.dao.DaoUser;
 import model.pojo.User;
@@ -8,6 +9,9 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Path("User")
 public class UserAPI extends RestApplication{
@@ -34,5 +38,41 @@ public class UserAPI extends RestApplication{
             response=Response.status(Response.Status.NO_CONTENT).entity(null).build();
         return response;
     }
-
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("CreateUser")
+    public Response createUser(@FormParam("firstname") String firstname,@FormParam("lastname") String lastname,@FormParam("email") String email,@FormParam("password") String password,@FormParam("address") String address,@FormParam("birthday") String birthday,@FormParam("registerDate") String registerDate,@FormParam("relationship") String relationship,@FormParam("phoneNumber") String phoneNumber,@FormParam("gender") String gender,@FormParam("interestedIn") String interestedIn){
+        Connection conn=GetConnection.getInstance().getConnection();
+        User u=new User();
+        u.setFirstname(firstname);
+        u.setLastname(lastname);
+        u.setEmail(email);
+        u.setPassword(password);
+        u.setAddress(address);
+        Date birth=null;
+        try {
+            birth=new SimpleDateFormat("dd/MM/yyyy").parse(birthday);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        u.setBirthday(birth);
+        Date register=null;
+        try {
+            register=new SimpleDateFormat("dd/MM/yyyy").parse(registerDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        u.setRegisterDate(register);
+        u.setRelationship(Boolean.parseBoolean(relationship));
+        u.setPhoneNumber(Integer.parseInt(phoneNumber));
+        u.setGender(Boolean.parseBoolean(gender));
+        u.setInterestedIn(Boolean.parseBoolean(interestedIn));
+        Boolean test=new DaoUser(conn).create(u);
+        Response response=null;
+        if(test)
+            response=Response.status(Response.Status.OK).entity(test).build();
+        else
+            response=Response.status(Response.Status.BAD_REQUEST).entity(null).build();
+        return response;
+    }
 }
