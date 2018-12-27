9630a3dd187b523d5b8e5c0c03aa246719f87767
thelittlewozniak <nathan.pire@gmail.com>
2018-12-27 15:04:55 +0100

    makig the GetAll user

--- Facebook-API/src/access/User.java
+++ Facebook-API/src/access/User.java
@@ -1,23 +1,35 @@
 package access;
 
+import jdk.net.SocketFlow;
+import model.BusinessLayer.GetConnection;
+import model.dao.DaoUser;
+
 import javax.print.attribute.standard.Media;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("User")
 public class User extends RestApplication{
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getText(){
-        return new String("Salut");
-    }
-    @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getTest() throws SQLException,ClassNotFoundException {
-        return new Test().testText();
+    public Response getAll(){
+        Connection conn= GetConnection.getInstance().getConnection();
+        Response response=null;
+        List<model.pojo.User> list=new ArrayList<>();
+        model.pojo.User user= new model.pojo.User();
+        user.setFirstname("Nathan");
+        list.add(user);
+        //response= Response.status(Response.Status.OK).entity(new DaoUser(conn).getAll()).build();
+        response= Response.status(Response.Status.OK).entity(list).build();
+        return response;
     }
+
 }
