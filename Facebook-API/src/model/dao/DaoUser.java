7dec1cbb363af70a7545c52e2a2f05dfd2998eac
thelittlewozniak <nathan.pire@gmail.com>
2018-12-28 22:08:46 +0100

    finishing DAOUser

--- Facebook-API/src/model/dao/DaoUser.java
+++ Facebook-API/src/model/dao/DaoUser.java
@@ -35,21 +35,83 @@ public class DaoUser extends Dao<User> {
             stmt.setInt(11,obj.getGender()?1:0);
             stmt.setInt(12,obj.getInterestedIn()?1:0);
             stmt.execute();
-            return stmt.getInt(1)!=-1?true:false;
+            return stmt.getInt(1)!=-1;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
+        }
+        finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     @Override
     public boolean delete(User obj) {
-        return false;
+        CallableStatement stmt=null;
+        try{
+            stmt=connect.prepareCall("{call USERPACKAGE.del(?)}");
+            stmt.setInt(1,obj.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        finally{
+            if(stmt!=null){
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        return true;
+        }
     }
 
     @Override
     public boolean update(User obj) {
-        return false;
+        CallableStatement stmt = null;
+        try {
+            stmt = connect.prepareCall("{call USERPACKAGE.upd(?,?,?,?,?,?,?,?,?,?,?,?)}");
+            stmt.setInt(1, obj.getId());
+            stmt.setString(2, obj.getFirstname());
+            stmt.setString(3, obj.getLastname());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getPassword());
+            stmt.setString(6, obj.getAddress());
+            stmt.setDate(7, java.sql.Date.valueOf(obj.getBirthday().toInstant().atZone(ZoneId.of("Europe/Brussels")).toLocalDate()));
+            stmt.setDate(8, java.sql.Date.valueOf(obj.getRegisterDate().toInstant().atZone(ZoneId.of("Europe/Brussels")).toLocalDate()));
+            stmt.setInt(9, obj.getRelationship() ? 1 : 0);
+            stmt.setInt(10, obj.getPhoneNumber());
+            stmt.setInt(11, obj.getGender() ? 1 : 0);
+            stmt.setInt(12, obj.getInterestedIn() ? 1 : 0);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            return true;
+        }
     }
 
     @Override
@@ -72,14 +134,30 @@ public class DaoUser extends Dao<User> {
                 u.setAddress(resultSet.getString(6));
                 u.setBirthday(resultSet.getTimestamp(7));
                 u.setRegisterDate(resultSet.getTimestamp(8));
-                u.setRelationship(resultSet.getInt(9)==1?true:false);
+                u.setRelationship(resultSet.getInt(9)==1);
                 u.setPhoneNumber(resultSet.getInt(10));
-                u.setGender(resultSet.getInt(11)==1?true:false);
-                u.setInterestedIn(resultSet.getInt(12)==1?true:false);
+                u.setGender(resultSet.getInt(11)==1);
+                u.setInterestedIn(resultSet.getInt(12)==1);
             }
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
+        }
+        finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return u;
     }
@@ -105,10 +183,10 @@ public class DaoUser extends Dao<User> {
                     u.setAddress(resultSet.getString(6));
                     u.setBirthday(resultSet.getTimestamp(7));
                     u.setRegisterDate(resultSet.getTimestamp(8));
-                    u.setRelationship(resultSet.getInt(9)==1?true:false);
+                    u.setRelationship(resultSet.getInt(9)==1);
                     u.setPhoneNumber(resultSet.getInt(10));
-                    u.setGender(resultSet.getInt(11)==1?true:false);
-                    u.setInterestedIn(resultSet.getInt(12)==1?true:false);
+                    u.setGender(resultSet.getInt(11)==1);
+                    u.setInterestedIn(resultSet.getInt(12)==1);
                     users.add(u);
                 }
             }
@@ -116,6 +194,22 @@ public class DaoUser extends Dao<User> {
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
+        }
+        finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return users;
     }
