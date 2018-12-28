c1ccdd6a0a78083399b9129db211721cc84bc222
thelittlewozniak <nathan.pire@gmail.com>
2018-12-28 09:12:35 +0100

    Making the methode Get with id for CommenAPI

--- Facebook-API/src/access/CommentAPI.java
+++ Facebook-API/src/access/CommentAPI.java
@@ -2,10 +2,12 @@ package access;
 
 import model.BusinessLayer.GetConnection;
 import model.dao.DaoComment;
+import model.pojo.Comment;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.Connection;
@@ -18,6 +20,19 @@ public class CommentAPI extends RestApplication {
     public Response getAll(){
         Connection conn= GetConnection.getInstance().getConnection();
         Response response= Response.status(Response.Status.OK).entity(new DaoComment(conn).getAll()).build();
+        return response;
+    }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("GetComment")
+    public Response getComment(@QueryParam("id") int id) {
+        Response response=null;
+        Connection conn=GetConnection.getInstance().getConnection();
+        Comment c=new DaoComment(conn).find(id);
+        if(c!=null)
+            response=Response.status(Response.Status.OK).entity(c).build();
+        else
+            response=Response.status(Response.Status.NO_CONTENT).entity(null).build();
         return response;
     }
 }
