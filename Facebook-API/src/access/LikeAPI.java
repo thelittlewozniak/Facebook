8cfddf580aff811750ca7ea9c2e6b1c37547e996
thelittlewozniak <nathan.pire@gmail.com>
2018-12-28 11:54:51 +0100

    making the method CreateLike for LikeAPI

--- Facebook-API/src/access/LikeAPI.java
+++ Facebook-API/src/access/LikeAPI.java
@@ -1,18 +1,20 @@
 package access;
 
 import model.BusinessLayer.GetConnection;
+import model.dao.DaoComment;
 import model.dao.DaoLike;
 import model.dao.DaoPost;
+import model.dao.DaoUser;
 import model.pojo.Like;
 import model.pojo.Post;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Path("Like")
 public class LikeAPI extends RestApplication {
@@ -27,7 +29,7 @@ public class LikeAPI extends RestApplication {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("getLike")
-    public Response getPost(@QueryParam("id") int id) {
+    public Response getLike(@QueryParam("id") int id) {
         Response response=null;
         Connection conn=GetConnection.getInstance().getConnection();
         Like l=new DaoLike(conn).find(id);
@@ -37,5 +39,31 @@ public class LikeAPI extends RestApplication {
             response=Response.status(Response.Status.NO_CONTENT).entity(null).build();
         return response;
     }
-    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("CreatePost")
+    public Response createLike(@FormParam("dateLiked") String dateLike,@FormParam("user") String userId,@FormParam("post") String postId,@FormParam("comment") String commentid){
+        Connection conn=GetConnection.getInstance().getConnection();
+        Like l=new Like();
+        l.setUser(new DaoUser(conn).find(Integer.parseInt(userId)));
+        Date date=null;
+        try {
+            date=new SimpleDateFormat("dd/MM/yyyy").parse(dateLike);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        l.setDateLiked(date);
+        if(postId.equals("null")){
+            l.setComment(new DaoComment(conn).find(Integer.parseInt(commentid)));
+        }
+        else
+            l.setPost(new DaoPost(conn).find(Integer.parseInt(postId)));
+        Boolean test=new DaoLike(conn).create(l);
+        Response response=null;
+        if(test)
+            response=Response.status(Response.Status.OK).entity(test).build();
+        else
+            response=Response.status(Response.Status.BAD_REQUEST).entity(test).build();
+        return response;
+    }
 }
