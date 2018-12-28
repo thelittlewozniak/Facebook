b5347b2345113f620d30d08762dcef41b114538f
thelittlewozniak <nathan.pire@gmail.com>
2018-12-28 10:24:47 +0100

    Making thz method GetAll for FriendAPI

--- Facebook-API/src/access/FriendAPI.java
+++ Facebook-API/src/access/FriendAPI.java
@@ -1,7 +1,23 @@
 package access;
 
+import model.BusinessLayer.GetConnection;
+import model.dao.DaoFriend;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.Connection;
 
 @Path("Friend")
 public class FriendAPI extends RestApplication {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("GetAll")
+    public Response getAll(){
+        Connection conn= GetConnection.getInstance().getConnection();
+        Response response= Response.status(Response.Status.OK).entity(new DaoFriend(conn).getAll()).build();
+        return response;
+    }
 }
