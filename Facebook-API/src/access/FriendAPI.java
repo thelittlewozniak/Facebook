b700b64b64161b57d106dd02c577ab72f83d57bc
thelittlewozniak <nathan.pire@gmail.com>
2018-12-28 10:41:23 +0100

    making the Method CreateFriend for FriendAPI

--- Facebook-API/src/access/FriendAPI.java
+++ Facebook-API/src/access/FriendAPI.java
@@ -3,15 +3,17 @@ package access;
 import model.BusinessLayer.GetConnection;
 import model.dao.DaoFriend;
 import model.dao.DaoPost;
+import model.dao.DaoUser;
 import model.pojo.Friend;
+import model.pojo.Post;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Path("Friend")
 public class FriendAPI extends RestApplication {
@@ -25,7 +27,7 @@ public class FriendAPI extends RestApplication {
     }
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("getFriend")
+    @Path("GetFriend")
     public Response getFriend(@QueryParam("id") int id) {
         Response response=null;
         Connection conn=GetConnection.getInstance().getConnection();
@@ -34,6 +36,22 @@ public class FriendAPI extends RestApplication {
             response=Response.status(Response.Status.OK).entity(f).build();
         else
             response=Response.status(Response.Status.NO_CONTENT).entity(null).build();
+        return response;
+    }
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("CreateFriend")
+    public Response createFriend(@FormParam("asker") String askerid,@FormParam("receiver") String receiverid,@FormParam("accepted") String accepted){
+        Connection conn=GetConnection.getInstance().getConnection();
+        Friend f=new Friend();
+        f.setAsker(new DaoUser(conn).find(Integer.parseInt(askerid)));
+        f.setReceiver(new DaoUser(conn).find(Integer.parseInt(receiverid)));
+        Boolean test=new DaoFriend(conn).create(f);
+        Response response=null;
+        if(test)
+            response=Response.status(Response.Status.OK).entity(test).build();
+        else
+            response=Response.status(Response.Status.BAD_REQUEST).entity(test).build();
         return response;
     }
 }
