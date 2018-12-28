29778cd592c160ad13ab4fc5c9ff7c831961fe8c
thelittlewozniak <nathan.pire@gmail.com>
2018-12-28 11:39:24 +0100

    making the method GetAll for LikeAPI

--- Facebook-API/src/access/LikeAPI.java
+++ Facebook-API/src/access/LikeAPI.java
@@ -1,8 +1,23 @@
 package access;
 
+import model.BusinessLayer.GetConnection;
+import model.dao.DaoLike;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.Connection;
 
 @Path("Like")
 public class LikeAPI extends RestApplication {
-
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("GetAll")
+    public Response getAll(){
+        Connection conn= GetConnection.getInstance().getConnection();
+        Response response= Response.status(Response.Status.OK).entity(new DaoLike(conn).getAll()).build();
+        return response;
+    }
 }
