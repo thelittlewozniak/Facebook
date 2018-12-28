3dce8d5d9bef2ae3932cffe67b523dc97b94b375
thelittlewozniak <nathan.pire@gmail.com>
2018-12-28 11:48:03 +0100

    Making method getLike for LikeAPI

--- Facebook-API/src/access/LikeAPI.java
+++ Facebook-API/src/access/LikeAPI.java
@@ -2,10 +2,14 @@ package access;
 
 import model.BusinessLayer.GetConnection;
 import model.dao.DaoLike;
+import model.dao.DaoPost;
+import model.pojo.Like;
+import model.pojo.Post;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.Connection;
@@ -20,4 +24,18 @@ public class LikeAPI extends RestApplication {
         Response response= Response.status(Response.Status.OK).entity(new DaoLike(conn).getAll()).build();
         return response;
     }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("getLike")
+    public Response getPost(@QueryParam("id") int id) {
+        Response response=null;
+        Connection conn=GetConnection.getInstance().getConnection();
+        Like l=new DaoLike(conn).find(id);
+        if(l!=null)
+            response=Response.status(Response.Status.OK).entity(l).build();
+        else
+            response=Response.status(Response.Status.NO_CONTENT).entity(null).build();
+        return response;
+    }
+    
 }
