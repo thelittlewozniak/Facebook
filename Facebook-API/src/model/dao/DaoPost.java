8664240eb3fbf257a071a332ff28edc568f000f8
thelittlewozniak <nathan.pire@gmail.com>
2018-12-29 10:21:28 +0100

    editing daopost to add methods to work with db

--- Facebook-API/src/model/dao/DaoPost.java
+++ Facebook-API/src/model/dao/DaoPost.java
@@ -1,8 +1,15 @@
 package model.dao;
 
 import model.pojo.Post;
+import model.pojo.User;
+import oracle.jdbc.internal.OracleTypes;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DaoPost extends Dao<Post> {
@@ -12,26 +19,171 @@ public class DaoPost extends Dao<Post> {
 
     @Override
     public boolean create(Post obj) {
-        return false;
+        CallableStatement stmt=null;
+        ResultSet resultSet=null;
+        try{
+            stmt=connect.prepareCall("{? = call POSTPACKAGE.add(?,?,?,?)}");
+            stmt.registerOutParameter(1,OracleTypes.NUMBER);
+            stmt.setString(2,obj.getData());
+            stmt.setString(3,obj.getType());
+            stmt.setDate(4,java.sql.Date.valueOf(obj.getPostDate().toInstant().atZone(ZoneId.of("Europe/Brussels")).toLocalDate()));
+            stmt.setInt(5,obj.getUser().getId());
+            stmt.execute();
+            return stmt.getInt(1)!=-1;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
     public boolean delete(Post obj) {
-        return false;
+        CallableStatement stmt=null;
+        try{
+            stmt=connect.prepareCall("{call POSTPACKAGE.del(?)}");
+            stmt.setInt(1,obj.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        finally{
+            if(stmt!=null){
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            return true;
+        }
     }
 
     @Override
     public boolean update(Post obj) {
-        return false;
+        CallableStatement stmt = null;
+        try {
+            stmt = connect.prepareCall("{call POSTPACKAGE.upd(?,?,?,?,?)}");
+            stmt.setInt(1, obj.getId());
+            stmt.setString(2,obj.getData());
+            stmt.setString(3,obj.getType());
+            stmt.setDate(4,java.sql.Date.valueOf(obj.getPostDate().toInstant().atZone(ZoneId.of("Europe/Brussels")).toLocalDate()));
+            stmt.setInt(5,obj.getUser().getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            return true;
+        }
     }
 
     @Override
     public Post find(int id) {
-        return null;
+        CallableStatement stmt=null;
+        Post p=new Post();
+        ResultSet resultSet=null;
+        try{
+            stmt=connect.prepareCall("{? = call POSTPACKAGE.get(?)}");
+            stmt.registerOutParameter(1, OracleTypes.CURSOR);
+            stmt.setInt(2, id);
+            stmt.execute();
+            resultSet=(ResultSet) stmt.getObject(1);
+            if(resultSet.next()){
+                p.setId(resultSet.getInt("idpost"));
+                p.setData(resultSet.getString("data"));
+                p.setType(resultSet.getString(3));
+                p.setPostDate(resultSet.getTimestamp(4));
+                p.setUser(new DaoUser(connect).find(resultSet.getInt(5)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return p;
     }
 
     @Override
     public List<Post> getAll() {
-        return null;
+        CallableStatement stmt=null;
+        List<Post> posts=new ArrayList<>();
+        ResultSet resultSet=null;
+        try{
+            stmt=connect.prepareCall("{? = call POSTPACKAGE.getAll}");
+            stmt.registerOutParameter(1,OracleTypes.CURSOR);
+            stmt.execute();
+            resultSet=(ResultSet) stmt.getObject(1);
+            if(resultSet!=null){
+                while(resultSet.next()){
+                    Post p=new Post();
+                    p.setId(resultSet.getInt("idpost"));
+                    p.setData(resultSet.getString("data"));
+                    p.setType(resultSet.getString(3));
+                    p.setPostDate(resultSet.getTimestamp(4));
+                    p.setUser(new DaoUser(connect).find(resultSet.getInt(5)));
+                    posts.add(p);
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return posts;
     }
 }
