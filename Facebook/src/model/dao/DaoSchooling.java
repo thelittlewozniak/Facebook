c8a481488dcdc84620b25b76bff725b3fd0910a2
thelittlewozniak <nathan.pire@gmail.com>
2019-01-01 15:42:07 +0100

    finishing editing DaoSchooling

--- Facebook/src/model/dao/DaoSchooling.java
+++ Facebook/src/model/dao/DaoSchooling.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 import model.pojo.Schooling;
+import model.pojo.Work;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DaoSchooling extends Dao<Schooling> {
@@ -51,7 +53,21 @@ public class DaoSchooling extends Dao<Schooling> {
 
     @Override
     public boolean update(Schooling obj) {
-        return false;
+        MultivaluedMap<String,String> params=new MultivaluedMapImpl();
+        ((MultivaluedMapImpl) params).add("schoolingId",obj.getId());
+        ((MultivaluedMapImpl) params).add("beginDate",obj.getBeginDate());
+        ((MultivaluedMapImpl) params).add("endDate",obj.getEndDate());
+        ((MultivaluedMapImpl) params).add("user",obj.getUser().getId());
+        ((MultivaluedMapImpl) params).add("graduate",obj.getGraduate());
+        String response = webResource.path("Schooling/UpdateSchooling").accept(MediaType.APPLICATION_JSON).type("application/x-www-form-urlencoded").put(String.class,params);
+        ObjectMapper mapper =new ObjectMapper();
+        Boolean done=false;
+        try {
+            done=mapper.readValue(response, new TypeReference<Boolean>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return done;
     }
 
     @Override
@@ -59,8 +75,28 @@ public class DaoSchooling extends Dao<Schooling> {
         return null;
     }
 
+    public Schooling find(int userId, int idSchool) {
+        String response = webResource.path("Schooling/getSchooling?iduser="+userId+"&id="+idSchool).accept(MediaType.APPLICATION_JSON).get(String.class);
+        ObjectMapper mapper =new ObjectMapper();
+        Schooling s=new Schooling();
+        try {
+            s=mapper.readValue(response, new TypeReference<Work>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return s;
+
+    }
     @Override
     public List<Schooling> getAll() {
-        return null;
+        String response = webResource.path("Schooling/GetAll").accept(MediaType.APPLICATION_JSON).get(String.class);
+        ObjectMapper mapper =new ObjectMapper();
+        List<Schooling> schoolings=new ArrayList<>();
+        try {
+            schoolings=mapper.readValue(response, new TypeReference<List<Schooling>>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return schoolings;
     }
 }
