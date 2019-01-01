b8f43b1c74bd6117d0fc60f2f2d5a7e0e1bba24a
thelittlewozniak <nathan.pire@gmail.com>
2019-01-01 14:24:20 +0100

    editing daoCommen but the branch doens't want to make it

--- Facebook/src/model/dao/DaoComment.java
+++ Facebook/src/model/dao/DaoComment.java
@@ -1,7 +1,13 @@
 package model.dao;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import model.pojo.Comment;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
 import java.sql.Connection;
 import java.util.List;
 
@@ -11,7 +17,22 @@ public class DaoComment extends Dao<Comment> {
     }
 
     @Override
-    public boolean create(Comment obj) { return false; }
+    public boolean create(Comment obj) {
+        MultivaluedMap<String,String> params=new MultivaluedMapImpl();
+        ((MultivaluedMapImpl) params).add("data",obj.getData());
+        ((MultivaluedMapImpl) params).add("type",obj.getType());
+        ((MultivaluedMapImpl) params).add("postDate",obj.getPostDate());
+        ((MultivaluedMapImpl) params).add("user",obj.getUser().getId());
+        String response = webResource.path("Post/CreatePost").accept(MediaType.APPLICATION_JSON).type("application/x-www-form-urlencoded").post(String.class,params);
+        ObjectMapper mapper =new ObjectMapper();
+        Boolean done=false;
+        try {
+            done=mapper.readValue(response, new TypeReference<Boolean>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return done;
+    }
 
     @Override
     public boolean delete(Comment obj) {
