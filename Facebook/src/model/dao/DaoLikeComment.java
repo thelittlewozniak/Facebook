1ed0d2268eeb39337a78de1fb2813dd99334fcbe
thelittlewozniak <nathan.pire@gmail.com>
2019-01-01 16:31:20 +0100

    making Create method

--- Facebook/src/model/dao/DaoLikeComment.java
+++ Facebook/src/model/dao/DaoLikeComment.java
@@ -1,7 +1,13 @@
 package model.dao;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import model.pojo.Like;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
 import java.util.List;
 
 public class DaoLikeComment extends Dao<Like> {
@@ -11,7 +17,19 @@ public class DaoLikeComment extends Dao<Like> {
 
     @Override
     public boolean create(Like obj) {
-        return false;
+        MultivaluedMap<String,String> params=new MultivaluedMapImpl();
+        ((MultivaluedMapImpl) params).add("dateLiked",obj.getDateLiked());
+        ((MultivaluedMapImpl) params).add("user",obj.getUser().getId());
+        ((MultivaluedMapImpl) params).add("comment",obj.getComment().getId());
+        String response = webResource.path("LikeComment/CreateLike").accept(MediaType.APPLICATION_JSON).type("application/x-www-form-urlencoded").post(String.class,params);
+        ObjectMapper mapper =new ObjectMapper();
+        Boolean done=false;
+        try {
+            done=mapper.readValue(response, new TypeReference<Boolean>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return done;
     }
 
     @Override
