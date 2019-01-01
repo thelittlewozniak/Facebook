df78b2107c219280912a22d237091dc27bf93ec0
thelittlewozniak <nathan.pire@gmail.com>
2019-01-01 14:51:15 +0100

    making DaoWork in same times

--- Facebook/src/model/dao/DaoWork.java
+++ Facebook/src/model/dao/DaoWork.java
@@ -1,7 +1,13 @@
 package model.dao;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import model.pojo.Work;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
 import java.util.List;
 
 public class DaoWork extends Dao<Work> {
@@ -11,17 +17,54 @@ public class DaoWork extends Dao<Work> {
 
     @Override
     public boolean create(Work obj) {
-        return false;
+        MultivaluedMap<String,String> params=new MultivaluedMapImpl();
+        ((MultivaluedMapImpl) params).add("name",obj.getName());
+        ((MultivaluedMapImpl) params).add("address",obj.getAddress());
+        ((MultivaluedMapImpl) params).add("beginDate",obj.getBeginDate());
+        ((MultivaluedMapImpl) params).add("endDate",obj.getEndDate());
+        ((MultivaluedMapImpl) params).add("user",obj.getUser().getId());
+        ((MultivaluedMapImpl) params).add("jobTitle",obj.getJobTitle());
+        String response = webResource.path("User/CreateUser").accept(MediaType.APPLICATION_JSON).type("application/x-www-form-urlencoded").post(String.class,params);
+        ObjectMapper mapper =new ObjectMapper();
+        Boolean done=false;
+        try {
+            done=mapper.readValue(response, new TypeReference<Boolean>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return done;
     }
 
     @Override
     public boolean delete(Work obj) {
-        return false;
+        String response = webResource.path("Work/DeleteWork?user="+obj.getUser().getId()+"&company="+obj.getId()).accept(MediaType.APPLICATION_JSON).delete(String.class);
+        ObjectMapper mapper =new ObjectMapper();
+        Boolean done=false;
+        try {
+            done=mapper.readValue(response, new TypeReference<Boolean>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return done;
     }
 
     @Override
     public boolean update(Work obj) {
-        return false;
+        MultivaluedMap<String,String> params=new MultivaluedMapImpl();
+        ((MultivaluedMapImpl) params).add("workid",obj.getId());
+        ((MultivaluedMapImpl) params).add("beginDate",obj.getBeginDate());
+        ((MultivaluedMapImpl) params).add("endDate",obj.getEndDate());
+        ((MultivaluedMapImpl) params).add("user",obj.getUser().getId());
+        ((MultivaluedMapImpl) params).add("jobTitle",obj.getJobTitle());
+        String response = webResource.path("User/UpdateWork").accept(MediaType.APPLICATION_JSON).type("application/x-www-form-urlencoded").post(String.class,params);
+        ObjectMapper mapper =new ObjectMapper();
+        Boolean done=false;
+        try {
+            done=mapper.readValue(response, new TypeReference<Boolean>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return done;
     }
 
     @Override
