1475e162cbd89addf152b194004c7e20f26beaa8
thelittlewozniak <nathan.pire@gmail.com>
2019-01-06 16:45:53 +0100

    creating the servlet to add a work

---
+++ Facebook/src/Controller/WorkAddServlet.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import model.pojo.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "WorkAddServlet")
+public class WorkAddServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = null;
+        User u = null;
+        HttpSession session = request.getSession();
+        if (request.getParameter("submit") != null && session.getAttribute("user") != null) {
+            u=(User) session.getAttribute("user");
+            u.addAWork(request.getParameter("name"),request.getParameter("address"),request.getParameter("begindate"),request.getParameter("jobtitle"));
+            response.sendRedirect("/Facebook_intelliJ_war_exploded/Profile/");
+        }
+        else
+            response.sendRedirect("/Facebook_intelliJ_war_exploded/Index/");
+    }
+}
